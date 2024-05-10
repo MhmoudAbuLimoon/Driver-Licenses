da7dc49c20080d3d2645732eb687c91186d0ff8e
MhmoudAbuLimoon <165291829+MhmoudAbuLimoon@users.noreply.github.com>
2024-05-10 10:54:44 -0700

    Add files via upload
    The First Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,58 @@
+﻿namespace DrivingLisenceApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ctrlPersonCard1 = new DrivingLisenceApp.Pepole.Control.ctrlPersonCard();
+            this.SuspendLayout();
+            // 
+            // ctrlPersonCard1
+            // 
+            this.ctrlPersonCard1.Location = new System.Drawing.Point(18, 12);
+            this.ctrlPersonCard1.Name = "ctrlPersonCard1";
+            this.ctrlPersonCard1.Size = new System.Drawing.Size(852, 313);
+            this.ctrlPersonCard1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(882, 450);
+            this.Controls.Add(this.ctrlPersonCard1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Pepole.Control.ctrlPersonCard ctrlPersonCard1;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using DrivingLisenceApp.Pepole;
+using DrivingLisenceApp.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DrivingLisenceApp
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //  Application.Run(new Form1());
+            //  Application.Run(new frmAddUpdatePerson(1078));
+            // Application.Run(new frmFindPerson());
+            // Application.Run(new frmShowPersonInfo("N1"));
+            // Application.Run(new frmListPeople());
+            //  Application.Run(new frmAddUpdateUser());
+            //  Application.Run(new frmUserInfo(37));
+            // Application.Run(new frmChangePassword(37));
+           // Application.Run(new frmAddUpdatePerson());
+            Application.Run(new frmAddUpdatePerson());
+        }
+    }
+}
